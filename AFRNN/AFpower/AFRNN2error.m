7eca3b388c2a0b99253ea88613c9433430009d40
QingyiRen <126511977+QingyiRen@users.noreply.github.com>
2024-12-26 08:18:51 +0100

    Add files via upload

---
+++ AFRNN/AFpower/AFRNN2error.m
@@ -0,0 +1,24 @@
+function AFRNN2error(y0,tspan,gamma)
+% tspan=[0 4]; 
+%If gamma is 1, then the end-time is 10s for creating figures
+%If gamma is 10,then the end-time is 1s  for creating figures
+%If gamma is 100,then the end-time is 0.1s for creating figures
+options=odeset('Mass', @LIVEmatrixW, 'MStateDep', 'none');
+% gamma=1;
+% for iter=1:6
+%     y0=1*(0.5*rand(3,1)-ones(3,1));
+    [t,y]=ode45(@AF2_con_righthandside, tspan, y0, options, gamma);
+         total=length(t);
+    for i=1:total
+    ts=t(i);
+    W=LIVEmatrixW(ts,y);
+    u=LIVEvectorU(ts,y);
+    err=W*(y(i,:))'-u;
+    nerr(i)=norm(err,2);
+    end
+    plot(t,nerr(1:length(t)),'r--','linewidth',1.5);
+    xlabel('t(s)');
+%     legend('AFRNN2');
+    hold on;
+% end
+
